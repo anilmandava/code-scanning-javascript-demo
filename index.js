82204f6c3ef238320b3b351141b6578cff64d28c
Anil Mandava <anil@mandava.us>
2022-05-21 03:44:57 -0700

    test2

--- index.js
+++ index.js
@@ -275,6 +275,22 @@ exports.extract = function (cwd, opts) {
       })
     }
 
+    var onlink2 = function () {
+      if (win32) return next() // skip links on win for now before it can be tested
+      xfs.unlink(name, function () {
+        var srcpath = path.resolve(cwd, header.linkname)
+
+        xfs.link(srcpath, name, function (err) {
+          if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
+            stream = xfs.createReadStream(srcpath)
+            return onfile()
+          }
+
+          stat(err)
+        })
+      })
+    }
+
     var onfile = function () {
       var ws = xfs.createWriteStream(name)
       var rs = mapStream(stream, header)
